b4da486b326b866d46fed50f528f889db6f67edc
Talysson19 <144975498+Talysson19@users.noreply.github.com>
2024-10-16 23:00:22 -0300

    Adicionar arquivos de projeto.

---
+++ PII VIII/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace PII_VIII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(434, 73);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "CONEXÃO";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ PII VIII/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Neo4j.Driver;
+
+namespace PII_VIII
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            // URI e credenciais do Neo4j
+            var uri = "bolt://localhost:7687";  // Substitua pelo IP do servidor, se for remoto
+            var user = "neo4j";  // Usuário padrão ou personalizado
+            var password = "12345678";  // Sua senha do Neo4j
+
+            // Criando o driver para conexão
+            var driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+            var session = driver.AsyncSession();  // Inicia a sessão
+
+            try
+            {
+                // Testa a conexão tentando abrir a sessão
+                await session.ExecuteReadAsync(tx => tx.RunAsync("RETURN 1"));
+                MessageBox.Show("Conexão estabelecida com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                // Caso haja erro na conexão
+                MessageBox.Show($"Erro na conexão: {ex.Message}");
+            }
+            finally
+            {
+                // Fechando a sessão e o driver
+                await session.CloseAsync();
+                await driver.CloseAsync();
+            }
+        }
+
+
+
+    }
+}
+    
