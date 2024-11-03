495d6208b97aaff64a3a5333a858f6e7b57c5966
Gabriel Pacheco <ogabrielpacheco1@gmail.com>
2024-11-03 00:00:20 -0300

    Alteraçao final do endereco do Aluno

--- PII VIII/EndAluno.cs
+++ PII VIII/EndAluno.cs
@@ -1,33 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Neo4j.Driver;
+using System;
 using System.Windows.Forms;
 
 namespace PII_VIII
 {
     public partial class EndAluno : Form
     {
+        private readonly IDriver _driver;
+
+
         public EndAluno()
         {
             InitializeComponent();
-            this.WindowState = FormWindowState.Maximized;
+            this.WindowState = FormWindowState.Normal;
             this.FormBorderStyle = FormBorderStyle.Sizable;
+            _driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "EscolaCC"));
+
         }
 
         private void EndAluno_Load(object sender, EventArgs e)
         {
             this.BackColor = System.Drawing.Color.FromArgb(211, 211, 211);
-            this.WindowState = FormWindowState.Normal;
+
         }
 
-        private void btnSalvarEndA_Click(object sender, EventArgs e)
+        private async void btnSalvarEndA_Click(object sender, EventArgs e)
         {
-            this.Close();
+            string nome = txtNomeEndA.Text;
+            int numero = int.Parse(txtNumEndA.Text);
+            string cep = txtCEPEndA.Text;
+            string bairro = txtBairroEndA.Text;
+            string cidade = txtCidadeEndA.Text;
+            string estado = txtEstadoEndA.Text;
+
+            using (var session = _driver.AsyncSession())
+            {
+                try
+                {
+                    await session.ExecuteWriteAsync(async tx =>
+                    {
+                        // Query para inserir o nó no Neo4j
+                        var query = "CREATE (p:enderecoAluno {nome: $nome, numero: $numero, cep: $cep, bairro: $bairro, cidade: $cidade , estado: $estado})";
+                        var parameters = new
+                        {
+                            nome = nome,
+                            numero = numero,
+                            cep = cep,
+                            bairro = bairro,
+                            cidade = cidade,
+                            estado = estado
+                        };
+                        await tx.RunAsync(query, parameters);
+                    });
+
+                    MessageBox.Show("Dados salvos com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao salvar dados: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                this.Close();
+            }
         }
     }
 }
+
---
+++ PII VIII/Neo4jDataBase.cs
@@ -0,0 +1,58 @@
+﻿using Neo4j.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PII_VIII
+{
+
+    public class Neo4jDatabase
+    {
+        private readonly string uri;
+        private readonly string user;
+        private readonly string password;
+        private IDriver _driver;
+
+        public Neo4jDatabase(string uri, string user, string password)
+        {
+            this.uri = uri;
+            this.user = user;
+            this.password = password;
+        }
+
+        private async Task ConectarNeo4jAsync()
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+            var session = _driver.AsyncSession();
+            try
+            {
+                await session.ExecuteReadAsync(tx => tx.RunAsync("RETURN 1"));
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+        }
+
+        public async Task SaveAddressAsync(string nome, int numero, string cep, string bairro, string cidade, string estado)
+        {
+            await ConectarNeo4jAsync();
+            var session = _driver.AsyncSession();
+            try
+            {
+                await session.ExecuteWriteAsync(async tx =>
+                {
+                    await tx.RunAsync("CREATE (e:Endereco {nome: $nome, numero: $numero, cep: $cep, bairro: $bairro, cidade: $cidade, estado: $estado})",
+                        new { nome, numero, cep, bairro, cidade, estado });
+                });
+            }
+            finally
+            {
+                await session.CloseAsync();
+                await _driver.CloseAsync();
+            }
+        }
+    }
+}
